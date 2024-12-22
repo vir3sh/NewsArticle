43d2c7da0d42d4f050dc1ad8ad410747afc59176
vir3sh <pviresh508@gmail.com>
2024-12-22 20:48:48 +0530

    added vercel json

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // Frontend URL
+    origin: "*", // Frontend URL
     credentials: true, // Allow credentials
   })
 );
